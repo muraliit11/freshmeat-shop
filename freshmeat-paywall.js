d2b079ea4d8820edc2d309ea31954f2bbf51b450
Murali <48654927+muraliit11@users.noreply.github.com>
2026-09-07 21:47:43 +0530

    Fix measurementId formatting and initialize Firebase
    Create freshmeat-paywall.js

---
+++ freshmeat-paywall.js
@@ -0,0 +1,14 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDoi4C6XNRMgyNNd4Ht3O33_EySe1Ked08",
+  authDomain: "freshmeat-96c63.firebaseapp.com",
+  projectId: "freshmeat-96c63",
+  storageBucket: "freshmeat-96c63.firebasestorage.app",
+  messagingSenderId: "1051584393564",
+  appId: "1:1051584393564:web:22c2d620b33cbdeb225199",
+  measurementId: "G-80KFY9B5BT
+};
+
+// Initialize Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+const app = initializeApp(firebaseConfig);
